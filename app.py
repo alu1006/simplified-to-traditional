44142f26144de0ad8bde20232e672bb2417ebd75
alu1006 <andy8050802@gmail.com>
2023-03-22 07:24:45 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, send_file
+import opencc
+import os
+import zipfile
+import tempfile
+import shutil
+from docx2txt import process as docx2txt
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    files = request.files.getlist('files[]')
+    temp_dir = tempfile.mkdtemp()
+
+    converted_files = []
+
+    for file in files:
+        filename, ext = os.path.splitext(file.filename)
+        file_path = os.path.join(temp_dir, file.filename)
+        file.save(file_path)
+
+        if ext.lower() == '.docx':
+            text = docx2txt(file_path)
+            with open(os.path.join(temp_dir, filename + '.txt'), 'w') as txt_file:
+                txt_file.write(text)
+
+        with open(file_path, 'r', encoding='utf-8', errors='ignore') as src:
+            content = src.read()
+            converter = opencc.OpenCC('s2t.json')
+            traditional = converter.convert(content)
+            converted_file_path = os.path.join(temp_dir, f"{filename}_convert{ext}")
+            with open(converted_file_path, "w") as dest:
+                dest.write(traditional)
+            converted_files.append(converted_file_path)
+
+    zip_file_path = os.path.join(temp_dir, 'converted_files.zip')
+    with zipfile.ZipFile(zip_file_path, 'w', compression=zipfile.ZIP_DEFLATED) as archive:
+        for converted_file in converted_files:
+            archive.write(converted_file, os.path.basename(converted_file))
+
+    response = send_file(zip_file_path, as_attachment=True)
+    shutil.rmtree(temp_dir)
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,53 @@
+# 簡轉繁應用程式
+
+一個用於將簡體中文文件轉換為繁體中文的Web應用程式。
+
+## 安裝
+
+1. 克隆此存儲庫：
+
+   ```git
+   git clone https://github.com/your_username/your_repository.git
+   ```
+2. 進入項目文件夾：
+
+   ```
+   cd your_repository
+   ```
+3. 創建一個虛擬環境：
+
+   ```
+   python3 -m venv venv
+   ```
+
+<pre><div class="bg-black rounded-md mb-4"><div class="flex items-center relative text-gray-200 bg-gray-800 px-4 py-2 text-xs font-sans justify-between rounded-t-md"></div></div><br class="Apple-interchange-newline"/></pre>
+
+4. 激活虛擬環境：
+
+- 對於Windows：
+
+  ```
+  venv\Scripts\activate
+  ```
+- 對於Linux和macOS：
+
+  ```
+  source venv/bin/activate
+  ```
+
+5. 安裝所需的依賴項：
+   ```
+   pip install -r requirements.txt
+   ```
+
+
+## 運行
+
+1. 運行Flask應用：
+   ```
+   python app.py
+
+
+   ```
+
+2. 在瀏覽器中訪問 [http://127.0.0.1:5000/](http://127.0.0.1:5000/) 以查看應用程式。
